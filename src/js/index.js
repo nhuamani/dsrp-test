653591ae1534fd351bb1d7630735228101385fa6
nhuamani <nhuamani.c7@gmail.com>
2021-06-19 21:51:37 -0500

    Mostrar/ocultar radio button

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,18 @@ import {activeMenu} from './modules/active-menu'
 // despedida()
 activeMenu()
 
-let checket = document.querySelectorAll('input[name="whatsapp"]');
-console.log(checket, "este es:");
+let contact = document.getElementById('contact')
 
+document.addEventListener('click', ()=> {
+  let checket = document.querySelectorAll('input[type="radio"]')
+  console.log(checket);
+
+  for (let i = 0; i < checket.length; i++) {
+    if (checket[i].checked && checket[i].value == 'si') {
+      contact.style.display = "block"
+    } else if(checket[i].checked && checket[i].value == 'no') {
+      contact.style.display = "none"
+    }
+  }
+
+})
